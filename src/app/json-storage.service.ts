421ad7e2cbcff34541e91b61c041c8e2dc154e44
Diego Campos <camposbuenodiego1@gmail.com>
2023-11-28 09:09:08 -0500

    "Final V1.0"

--- src/app/json-storage.service.ts
+++ src/app/json-storage.service.ts
@@ -4,20 +4,5 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class JsonStorageService {
-  private jsonData: string="";
 
-  setJsonData(data: string) {
-    this.jsonData = data;
-  }
-
-  getJsonData(): string {
-    return this.jsonData;
-  }
-
-  // Agrega un método para obtener la imagen a partir de la cadena JSON guardada
-  getImageFromJson(): string {
-    // Analiza la cadena JSON y obtén la URL base64 de la imagen
-    const parsedData = JSON.parse(this.jsonData);
-    return parsedData.imageDataURL; // Asegúrate de adaptar esto a tu estructura de datos.
-  }
 }
--- src/app/service/question.service.ts
+++ src/app/service/question.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 
-const basic_url=['http://localhost:8080']
 
 @Injectable({
   providedIn: 'root'
@@ -11,35 +9,12 @@ export class QuestionService {
 
   constructor(private http: HttpClient) { }
 
-  getQuestionJson() {
-    return this.http.get<any>('assets/questions.json');
-  }
-
   getResultadosJson() {
     return this.http.get<any>('assets/resultados.json');
-  }
-
-  getLibrosJson() {
-    return this.http.get<any>('assets/libros.json')
-      .pipe(
-        map(res => {
-          if (res && res.libros) {
-            // Agregar la propiedad portadaUrl a cada libro
-            res.libros.forEach((libro: any) => {
-              libro.portadaUrl = this.convertirBase64ToUrl(libro.portada);
-            });
-          }
-          return res;
-        })
-      );
   }
 
   getTitautJson() {
     return this.http.get<any>('assets/titaut.json');
   }
 
-  private convertirBase64ToUrl(base64String: string): string {
-    // Convertir cadena base64 a URL de datos
-    return base64String;
-  }
 }
